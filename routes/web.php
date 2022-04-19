357345f593128af3548064cb0d912142bdd9637d
Davis Too <daviskipkiruitoo@gmail.com>
2022-04-19 17:30:08 +0300

    created project base file structure

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,32 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
+
+
+
+Route::group(
+    [
+        'namespace'     =>  'AidynMakhataev\LaravelSurveyJs\app\Http\Controllers',
+        'middleware'    =>  config('survey-manager.route_middleware'),
+        'prefix'        =>  config('survey-manager.route_prefix'),
+    ],
+    function () {
+        Route::get('/{surveySlug}', 'SurveyController@runSurvey')->name('survey-manager.run');
+    }
+);
+
+Route::group(
+    [
+        'namespace'     =>  'AidynMakhataev\LaravelSurveyJs\app\Http\Controllers',
+        'prefix'        =>  config('survey-manager.admin_prefix').'/survey/',
+        'middleware'    =>  config('survey-manager.admin_middleware'),
+    ],
+    function () {
+        Route::view('{vue?}', 'survey-manager::admin')->where('vue', '[\/\w\.-]*')->name('survey-manager.admin');
+    }
+);
